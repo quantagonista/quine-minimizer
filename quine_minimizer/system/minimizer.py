8fff0c6aaf524823b6ac1280d815edbd4160f0e3
quanta <quantagonista@gmail.com>
2018-06-06 23:56:28 +0600

    Save Us

--- quine_minimizer/system/minimizer.py
+++ quine_minimizer/system/minimizer.py
@@ -6,6 +6,59 @@ class Minimizer():
         self.count = count
         self.terms = [[int(i) for i in list("{number:0{base}b}".format(number=x, base=count))] for x in arguments]
 
+    def find_core_implicants(self, table):
+        core = []
+        width = len(table[0])
+        height = len(table)
+        columns = []
+        rows = []
+        coverage = []
+
+        # finding indices for FIRST core implicants
+        for j in range(width):
+            s = 0
+            for i in range(height):
+                s += table[i][j]
+            if s == 1:
+                columns.append(j)
+
+        # adding FC implicants and finding indices for them
+        for j in columns:
+            for i in range(height):
+                if table[i][j] == 1:
+                    implicant = self.primary_implicants[i]
+                    if implicant not in core:
+                        core.append(implicant)
+                        rows.append(i)
+
+        for i in rows:
+            coverage.append(sum(table[i]))
+        coverage = sum(coverage)
+        if coverage >= width:
+            # job done
+            return core
+        else:
+            pass
+
+        shortenned_table_one = []
+
+        for i in range(height):
+            if i not in rows:
+                shortenned_table_one.append(table[i])
+
+        shortenned_table_two = [[] for _ in range(len(shortenned_table_one))]
+        for i in range(len(shortenned_table_one)):
+            for j in range(width):
+                if j not in columns:
+                    shortenned_table_two[i].append(shortenned_table_one[i][j])
+
+        self.print_implicants(table)
+
+        print('SHORT:::')
+        self.print_implicants(shortenned_table_two)
+
+        return core
+
     def build_coverage_table(self):
         term_len = len(self.terms)
         impl_len = len(self.primary_implicants)
@@ -17,17 +70,19 @@ class Minimizer():
                 if self.is_in(implicants[i], terms[j]):
                     table[i][j] = 1
         return table
-        #self.print_coverage_table(table)
 
     def find_primary_implicants(self):
         primary_implicants = self.terms
         last = []
+        result = []
+
         while True:
             x = self.merge_implicants(primary_implicants)
             if x:
                 if x != last:
                     primary_implicants = self.merge_implicants(primary_implicants)
                     last = primary_implicants.copy()
+                    # result.extend(last)
                 else:
                     break
             else:
@@ -113,11 +168,11 @@ class Minimizer():
         head += '\n'
         rows = ''
         for i in range(len(self.primary_implicants)):
-            row = self.to_str(self.primary_implicants[i]) + '|' + self.to_str(table[i]) + '\n'
+            row = self.to_str(self.primary_implicants[i]) + '|\t' + '\t' + '        '.join(
+                [str(x) for x in table[i]]) + '\n'
             rows += row
         table = head + rows
         print(table)
-
 
     def to_str(self, array):
         return ''.join(map(lambda x: str(x), array))
